e1352600496587a18d6177b8099c60f17d9927a0
NoSoyTuMadre <tonimfuturelawson@gmail.com>
2021-02-16 16:26:01 -0500

    Created Movie class and retrieved movies and poster image's url

--- app/src/main/java/com/codepath/tonifields/flixster/MainActivity.java
+++ app/src/main/java/com/codepath/tonifields/flixster/MainActivity.java
@@ -7,10 +7,13 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.codepath.tonifields.flixster.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -18,7 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing" +
             "?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String POSTER_IMAGE_URL = "https://api.themoviedb.org/3/configuration" +
+            "?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
+    JSONArray results;
+    JSONObject images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +35,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AsyncHttpClient client = new AsyncHttpClient();
+        client.get(POSTER_IMAGE_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    // get base-url
+                    images = jsonObject.getJSONObject("images");
+                    Log.i(TAG, "Images: " + images.toString());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.d(TAG, "onSuccess");
                 JSONObject jsonObject = json.jsonObject;
                 try {
-                    JSONArray results = jsonObject.getJSONArray("results");
+                    results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results: " + results.toString());
+                    List<Movie> movies = Movie.fromJsonArray(results, images);
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit json exception", e);
                 }
@@ -45,4 +74,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-};
+}
---
+++ app/src/main/java/com/codepath/tonifields/flixster/models/Movie.java
@@ -0,0 +1,59 @@
+package com.codepath.tonifields.flixster.models;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+    String basePath;
+    String imageSize = "w342";
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.title = jsonObject.getString("title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public Movie(JSONObject jsonObject, Object object2) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.title = jsonObject.getString("title");
+        this.overview = jsonObject.getString("overview");
+        this.basePath = object2.toString();
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray, JSONObject posterJsonObject) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i), posterJsonObject.get("base_url")));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("%s%s%s", basePath, imageSize, posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getBasePath() {
+        return basePath;
+    }
+
+    public String getImageSize() {
+        return imageSize;
+    }
+}
