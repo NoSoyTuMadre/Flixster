274246b28213ae48abb2feeb4e0130089d0f0682
Toni_Fields <57774418+NoSoyTuMadre@users.noreply.github.com>
2021-02-24 01:05:49 -0500

    Update DetailActivity.java

--- app/src/main/java/com/codepath/tonifields/flixster/DetailActivity.java
+++ app/src/main/java/com/codepath/tonifields/flixster/DetailActivity.java
@@ -26,8 +26,8 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyDSrVkz4H37-oWk5pxemlRoUxEh6kp_EgQ";
-    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String YOUTUBE_API_KEY = "<put YouTube API key here";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=<put movies api key here>";
 
     TextView tvTitle;
     TextView tvOverview;
